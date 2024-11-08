da1ce49b59f0449d5a25b3e76898d4aa77a2da14
SJ3742 <56733732+SJ3742@users.noreply.github.com>
2024-11-08 09:45:42 +0900

    Update cardController.java

--- cardGenerator/src/test/java/com/card/mvc/controller/cardController.java
+++ cardGenerator/src/test/java/com/card/mvc/controller/cardController.java
@@ -1,5 +1,37 @@
 package com.card.mvc.controller;
 
-public class cardController {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.card.mvc.model.dto.Card;
+import com.card.mvc.service.cardService;
+
+@Controller
+@RequestMapping("/")
+public class cardController {
+	private cardService service;
+	public cardController(cardService service) {
+		this.service = service;
+	}
+	
+	//받아오고 카드 생성
+	@PostMapping
+	public ResponseEntity<Card> generateCard(@RequestBody Card card){
+        try {
+            // 카드 생성 로직
+            Card newCard = cardService.createCard(card);
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(newCard);
+        } catch (Exception e) {
+            // 실패 시 INTERNAL_SERVER_ERROR 반환
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+	}
+	
+	
+	
 }
