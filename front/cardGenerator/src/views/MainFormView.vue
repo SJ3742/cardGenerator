94afa91e6db299d1b7b66ac3e515d5b9147bb726
정석진 <patchouli6942@gmail.com>
2024-11-08 16:33:51 +0900

    폼 수정

--- front/cardGenerator/src/views/MainFormView.vue
+++ front/cardGenerator/src/views/MainFormView.vue
@@ -9,6 +9,7 @@
           <input type="url" id="url" class="form-control" placeholder="URL을 입력하세요" />
   
           <label for="type">타입</label>
+
           <!-- 선택된 옵션의 색상을 적용 -->
           <select 
             id="type" 
@@ -24,6 +25,15 @@
               {{ type.name }}
             </option>
           </select>
+          
+
+          <label for="skill1">스킬1</label>
+          <input type="text" id="skill1" class="form-control" placeholder="스킬을 입력하세요" />
+
+          <label for="skill2">스킬2</label>
+          <input type="text" id="skill2" class="form-control" placeholder="스킬을 입력하세요" />
+
+
         </div>
   
         <button type="submit" class="btn btn-primary mt-3">제출하기!!!</button>
@@ -35,14 +45,16 @@
   import { ref, computed } from 'vue';
   
   const types = ref([
-    { id: 1, name: 'GRASS', colorCode: '#78C850' },
-    { id: 2, name: 'WATER', colorCode: '#6890F0' },
-    { id: 3, name: 'FIRE', colorCode: '#F08030' },
-    { id: 4, name: 'LIGHTNING', colorCode: '#F8D030' },
-    { id: 5, name: 'DARK', colorCode: '#705848' },
-    { id: 6, name: 'STEEL', colorCode: '#B8B8D0' },
-    { id: 7, name: 'DRAGON', colorCode: '#7038F8' },
-    { id: 8, name: 'NORMAL', colorCode: '#A8A878' }
+    { id: 1, name: '풀', colorCode: '#78C850' },
+    { id: 2, name: '불', colorCode: '#F08030' },
+    { id: 3, name: '물', colorCode: '#6890F0' }, 
+    { id: 4, name: '전기', colorCode: '#F8D030' },
+    { id: 5, name: '초', colorCode: '#F85888' },
+    { id: 6, name: '격투', colorCode: '#C03028' },
+    { id: 7, name: '악', colorCode: '#705848' },
+    { id: 8, name: '강철', colorCode: '#B8B8D0' },
+    { id: 9, name: '드래곤', colorCode: '#7038F8' },
+    { id: 10, name: '무색', colorCode: '#A8A878' }
   ]);
   
   const selectedType = ref(types.value[0].name); // 초기 선택값 설정
